6199d197a6db8a7c52814b240a359abc46d32acb
Sajid Manzoor <sqaprofessional1@gmail.com>
2024-06-05 12:50:31 +0500

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,14 +3,13 @@ const { allureCypress } = require("allure-cypress/reporter");
 
 module.exports = defineConfig({
   projectId: "8199cm",
-  defaultCommandTimeout:40000,
+  defaultCommandTimeout: 40000,
   reporter: 'cypress-mochawesome-reporter',
   screenshotOnRunFailure: true,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
       allureCypress(on);
-      this.screenshotOnRunFailure = true;
       require('cypress-mochawesome-reporter/plugin')(on);
     },
     experimentalRunAllSpecs: true,
