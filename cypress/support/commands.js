980d455df5a78cc6f7cc729b868710efee377725
Sajid Manzoor <sqaprofessional1@gmail.com>
2024-06-05 11:46:44 +0500

    Script Update

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,15 +1,3 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
 import loginObjects from "../e2e/Objects/login";
 import utilities from "./utils";
 
@@ -19,20 +7,8 @@ const utils = new utilities();
 Cypress.Commands.add("login", (email, password) => {
   utils.navigateToUrl();
   utils.verifyVisibility(login.Auth.emailField());
-  utils.enterText(login.Auth.emailField(), email+'{enter}');
+  utils.enterText(login.Auth.emailField(), email + '{enter}');
   utils.verifyVisibility(login.Auth.passwordField());
-  utils.enterText(login.Auth.passwordField(), password+'{enter}');
+  utils.enterText(login.Auth.passwordField(), password + '{enter}');
   utils.verifyVisibility(login.Auth.orgName());
 });
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
